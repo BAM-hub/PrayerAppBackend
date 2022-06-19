d39b1daaeb204e9c58e0c19e54d65753cb8e7c10
BAM-hub <bsharamin12@gmail.com>
2022-06-19 19:14:21 +0300

    mutations changes related to types

--- src/resolvers/userResolver.ts
+++ src/resolvers/userResolver.ts
@@ -1,18 +1,29 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { UserType } from "../schemas/userSchema";
 import UserModel from "../models/UserModel";
+import { ObjectIdScalar } from "../myScalars/ObjectId";
+import { ObjectId } from "mongodb";
+import { type } from "os";
 
 @Resolver(UserType)
 export class UserResolver {
+  // find all users
+  @Query((returns) => [UserType])
+  async users(): Promise<UserType[] | undefined> {
+    return await UserModel.find({});
+  }
+
   // find user by id
-  @Query(() => UserType)
-  async user(@Arg("_id") _id: String): Promise<UserType | null> {
+  @Query((returns) => UserType)
+  async user(
+    @Arg("_id", (type) => ObjectIdScalar) _id: ObjectId
+  ): Promise<UserType | null> {
     return await UserModel.findById(_id);
   }
 
   // add user
-  @Mutation(() => UserType)
-  async addUser(@Arg("name") name: String) {
+  @Mutation((returns) => UserType)
+  async addUser(@Arg("name", (type) => String) name: String) {
     const user = new UserModel({
       name,
     });
@@ -23,7 +34,9 @@ export class UserResolver {
 
   // delete user
   @Mutation(() => UserType)
-  async deleteUser(@Arg("_id") _id: String) {
+  async deleteUser(
+    @Arg("_id", (type) => ObjectIdScalar) _id: ObjectId
+  ): Promise<null | UserType> {
     return await UserModel.findByIdAndDelete(_id);
   }
 }
