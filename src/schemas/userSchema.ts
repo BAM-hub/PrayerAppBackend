2ec98a803845fad60173d887dfde83d9244d4853
BAM-hub <bsharamin12@gmail.com>
2022-06-20 04:52:35 +0300

    added emaul and password fields and a type for user errors might use it

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -8,5 +8,41 @@ export class UserType {
   readonly _id: ObjectId;
 
   @Field((type) => String)
+  email: String;
+
+  @Field((type) => String)
+  password: String;
+
+  @Field((type) => String)
   name: String;
+}
+
+@ObjectType()
+class ErrorLocationsType {
+  @Field((type) => Number)
+  line: Number;
+
+  @Field((type) => Number)
+  column: Number;
+}
+
+@ObjectType()
+class ErrorType {
+  @Field((type) => String)
+  message: String;
+
+  @Field((type) => [String], { nullable: true })
+  path: String[];
+
+  @Field((type) => [ErrorLocationsType], { nullable: true })
+  locations: ErrorLocationsType[];
+}
+
+@ObjectType()
+export class UserErrorType {
+  @Field((type) => [ErrorType])
+  errors: [ErrorType];
+
+  @Field((type) => Object)
+  data: Object;
 }
