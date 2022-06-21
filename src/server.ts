45fd1bff1c50e02c076af020ca0560e6541bd022
BAM-hub <bsharamin12@gmail.com>
2022-06-21 05:18:38 +0300

    added record resolver

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import * as express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { buildSchema } from "type-graphql";
 
+import { RecordResolver } from "./resolvers/RecoerdResolver";
 import { UserResolver } from "./resolvers/userResolver";
 import connectDB from "./config/db";
 
@@ -11,7 +12,9 @@ const app = express();
 connectDB();
 
 const main = async () => {
-  const schema = await buildSchema({ resolvers: [UserResolver] });
+  const schema = await buildSchema({
+    resolvers: [UserResolver, RecordResolver],
+  });
   app.use(
     "/graphql",
     graphqlHTTP({
