a34406abf61767aa51f849200669f9a2209b76d4
BAM-hub <bsharamin12@gmail.com>
2022-06-21 05:19:18 +0300

    the record schema is added and subkect to change based on requirment

---
+++ src/models/RecordModel.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from "mongodb";
+import mongoose from "mongoose";
+import { RecordType } from "src/schemas/recordSchema";
+
+const RecordSchema = new mongoose.Schema({
+  userId: {
+    type: ObjectId,
+  },
+  score: {
+    type: Number,
+    default: 0,
+  },
+  prayers: {
+    type: [Number],
+    enum: [0, 1, 2, 3, 4, 5],
+    default: 0,
+  },
+  nuaffel: {
+    type: [Number],
+    enum: [0, 1, 2],
+    default: 0,
+  },
+});
+
+export default mongoose.model<RecordType>("Record", RecordSchema);
