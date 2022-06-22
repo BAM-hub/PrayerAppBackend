13702a96aa6b06b2a743a5211b51cd51a5e07424
BAM-hub <bsharamin12@gmail.com>
2022-06-22 05:41:37 +0300

    added a generic resolver for both check prayers and nuaffel still room for improvement

--- src/resolvers/RecoerdResolver.ts
+++ src/resolvers/RecoerdResolver.ts
@@ -1,9 +1,68 @@
 import { ObjectId } from "mongodb";
 import { ObjectIdScalar } from "../myScalars/ObjectId";
-import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, ClassType, Mutation, Query, Resolver } from "type-graphql";
 import { Nuaffel, Prayers, RecordType } from "../schemas/recordSchema";
 import UserModel from "../models/UserModel";
 import RecordModel from "../models/RecordModel";
+
+function createBaseResolver<T extends ClassType<RecordType>>(
+  suffix: String,
+  returnType: T,
+  inputType: any,
+  field: string
+  // entity: any
+) {
+  @Resolver((returns) => RecordType)
+  class BaseResolver {
+    @Mutation((returns) => returnType, { name: `check${suffix}` })
+    async check(
+      @Arg(`${suffix.toLocaleLowerCase()}`, (type) => inputType, {
+        nullable: true,
+      })
+      data: typeof inputType,
+      @Arg("_id", (type) => ObjectIdScalar) _id: ObjectId
+    ): Promise<T | null> {
+      const record = await RecordModel.findOneAndUpdate({ userId: _id }, [
+        {
+          $set: {
+            [field]: { $concatArrays: [`$${field}`, [data]] },
+          },
+        },
+        {
+          $set: {
+            score: { $add: [{ $sum: "$prayers" }, { $sum: "$nuaffel" }] },
+          },
+        },
+      ]);
+
+      if (record) return await RecordModel.findOne({ userId: _id });
+
+      const newRecord: any = new RecordModel({
+        userId: _id,
+        score: data,
+        [field]: [data],
+      });
+
+      return await newRecord.save();
+    }
+  }
+
+  return BaseResolver;
+}
+
+export const CheckPrayer = createBaseResolver(
+  "Prayer",
+  RecordType,
+  Prayers,
+  "prayers"
+);
+
+export const CheckNuaffel = createBaseResolver(
+  "Nuaffel",
+  RecordType,
+  Nuaffel,
+  "nuaffel"
+);
 
 @Resolver(RecordType)
 export class RecordResolver {
@@ -13,38 +72,5 @@ export class RecordResolver {
     @Arg("_id", (type) => ObjectIdScalar) _id: ObjectId
   ): Promise<RecordType | null> {
     return await UserModel.findById(_id);
-  }
-
-  @Mutation((returns) => RecordType)
-  async checkDeed(
-    @Arg("prayer", (type) => Prayers, { nullable: true }) prayer: Prayers,
-    @Arg("nuaffel", (type) => Nuaffel, { nullable: true }) nuaffel: Nuaffel,
-    @Arg("_id", (type) => ObjectIdScalar) _id: ObjectId
-  ) {
-    // hint might refactor to useing switch instead for fewer test cases
-    let record = await RecordModel.findOne({ userId: _id });
-
-    if (!record) {
-      let newRecord = new RecordModel({
-        userId: _id,
-        score: prayer ? prayer : nuaffel ? nuaffel : 0,
-      });
-
-      if (prayer) newRecord.prayers = [prayer];
-      if (nuaffel) newRecord.nuaffel = [nuaffel];
-
-      return await newRecord.save();
-    }
-
-    if (prayer) {
-      record.prayers = [...record.prayers, prayer];
-      record.score = record.score + prayer;
-    }
-    if (nuaffel) {
-      record.nuaffel = [...record.nuaffel, nuaffel];
-      record.score = record.score + nuaffel;
-    }
-
-    return await record.save();
   }
 }
